e9340a3515f3ae3089a3254fae60d71388191fc1
xamidev <maximebrunet2@protonmail.com>
2023-01-26 20:17:44 +0100

    added testing files for demonstration

--- testing.py
+++ testing.py
@@ -1,1 +1,16 @@
 print("Hello world")
+
+print("wassup")
+
+# i like python
+
+print("u sure?")
+
+"""
+i really enjoy tea.
+*sips*
+tea for two and two for tea
+me and you and you and me
+"""
+
+print("shoudln't we be, uh, doing something?")
